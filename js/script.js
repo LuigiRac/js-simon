d87d06f71da37693df2f90172802caf07c8b108c
LuigiRac <lui.rac06@gmail.com>
2024-11-12 19:25:26 +0100

    star exercise + function

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict"
+console.clear()
+
+function randomNumbers() {
+    for (let i = 0; i < array.length; i++) {
+        const casualNumber = Math.floor(Math.random() * 100) + 1;
+
+    }
+}
